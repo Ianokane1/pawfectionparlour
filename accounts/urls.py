49045177f1ecab6cfdd171eda4be632721c4cd29
Ian Kane <ianwilliamkane@gmail.com>
2022-05-25 19:24:11 +0000

    Add content to base.html and views for services and contact page

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')]
+    path('', views.HomeView.as_view(), name='home'),
+    path('services/', views.ServicesView.as_view(), name='services'),
+    path('contact_us/', views.ContactView.as_view(), name='contact'),
+    ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse, redirect
 # Import Django generic libary
 from django.views import generic, View
 from django.views.generic import TemplateView, DeleteView
-
+# Import Booking model from models
 from .models import Booking, UserProfile
 
 # Create your views here.
@@ -14,7 +14,22 @@ class HomeView(TemplateView):
         return render(
             request,
             "index.html",
-            {
-                "home_active": "",
-            }
+            )
+
+class ServicesView(TemplateView):
+    template_name = "services.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "services.html",
         )
+
+class ContactView(TemplateView):
+    template_name = "contact.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "contact.html",
+            )                    
