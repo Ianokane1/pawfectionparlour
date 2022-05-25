6c484afc832319c5b9b3f8144a6237de0bc4eea6
Ian Kane <ianwilliamkane@gmail.com>
2022-05-25 18:16:02 +0000

    Add homeview to views and path in urls to display index.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 # Import Django generic libary
 from django.views import generic, View
+from django.views.generic import TemplateView, DeleteView
 
 # Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "index.html",
+            {
+                "home_active": "",
+            }
+        )
