0e74ae75ea6235dc0d5969fa447e575b363c91a9
Ian Kane <ianwilliamkane@gmail.com>
2022-05-25 19:38:43 +0000

    add servcices page

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from .models import Booking, UserProfile
+from django import forms
+from django.forms import ModelForm, CharField, TextInput
+
+class UpdateBookingDetails(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = (
+            'booking_date', 'booking_time')
+
+class EditProfileForm(forms.ModelForm):
+
+    phone_number = CharField(widget=TextInput(attrs={'type':'number'}))
+    class Meta:
+        model = UserProfile
+        fields = ('first_name', 'last_name', 'dog_name', 'phone_number')            
